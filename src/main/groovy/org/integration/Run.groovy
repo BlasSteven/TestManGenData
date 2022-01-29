4c152d5cadce5b92ae9f7c92fd74d549b5172bb8
Blas Alarcon <stevenalarconr@outlook.com>
2022-01-29 01:14:20 -0500

    Se le agrgo un try al arranque

--- src/main/groovy/org/integration/Run.groovy
+++ src/main/groovy/org/integration/Run.groovy
@@ -3,12 +3,16 @@ package org.integration
 class Run {
 
     static void main(String[] args){
-        Propiedades props = new Propiedades()
-        props.loadPropsTestPlan("com_testPlans_TestLink")
-        TestManIntegration test = new TestManIntegration(props.getProyecto(),props.getPlanPruebas(),
-                props.getCasoPrueba(),props.getSuitePrueba(),props.getBuild(),props.getCategory(),
-                props.getAssigneTo(),false,"",1,
-                "com_testPlans_TestLink")
-        test.caseValidTest()
+        try {
+            Propiedades props = new Propiedades()
+            props.loadPropsTestPlan("com_testPlans_TestLink")
+            TestManIntegration test = new TestManIntegration(props.getProyecto(), props.getPlanPruebas(),
+                    props.getCasoPrueba(), props.getSuitePrueba(), props.getBuild(), props.getCategory(),
+                    props.getAssigneTo(), false, "", 1,
+                    "testlink")
+            test.caseValidTest()
+        }catch (Exception e){
+            e.printStackTrace()
+        }
     }
 }
